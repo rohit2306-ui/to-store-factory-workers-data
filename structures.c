6491f89b0c5b9c9f10b6c9b9342f3736ba574523
rohit2306-ui <140095379+rohit2306-ui@users.noreply.github.com>
2023-11-23 00:55:07 +0530

    Add files via upload

---
+++ structures.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<string.h>
+        struct rohit{
+        int code;
+        float salary;
+        char name[10];
+    };
+    
+int main(){
+        struct rohit r1;
+        r1.code;
+        printf("enter your code: \n");
+        scanf("%d",&r1.code);
+        r1.salary;
+        printf("enter your code: \n");
+        scanf("%f",&r1.salary);
+        r1.name;
+        printf("enter your name: \n");
+        scanf("%s", r1.name);
+        
+        printf("the value of code is:%d\n",r1.code);
+         printf("the value of code is:%f\n",r1.salary);
+          printf("the value of code is:%d\n",r1.code);
+        
+        
+        return 0;
+    }
+    
+    
