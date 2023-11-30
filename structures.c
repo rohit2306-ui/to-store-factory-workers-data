96302aac02675a03e5b8e26ea121b262884c244b
rohit thkaur <140095379+rohit2306-ui@users.noreply.github.com>
2023-11-30 20:03:56 +0530

    Update structures.c

--- structures.c
+++ structures.c
@@ -7,12 +7,16 @@
     };
     
 int main(){
+    int a;
+    printf("enetr your limit to sotre the data:\n");
+    scanf("%d",&a);
+    for(int i=0;i < a;i++){
         struct rohit r1;
         r1.code;
         printf("enter your code: \n");
         scanf("%d",&r1.code);
         r1.salary;
-        printf("enter your code: \n");
+        printf("enter your salary: \n");
         scanf("%f",&r1.salary);
         r1.name;
         printf("enter your name: \n");
@@ -21,8 +25,7 @@ int main(){
         printf("the value of code is:%d\n",r1.code);
          printf("the value of code is:%f\n",r1.salary);
           printf("the value of code is:%d\n",r1.code);
-        
-        
+        }    
         return 0;
     }
     
