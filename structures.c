e2d6734c67e208ea3f8546a3512c1c93d70fbe87
rohit thkaur <140095379+rohit2306-ui@users.noreply.github.com>
2023-12-01 12:29:53 +0530

    Update structures.c

--- structures.c
+++ structures.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<string.h>
+
         struct rohit{
         int code;
         float salary;
@@ -12,19 +13,19 @@ int main(){
     scanf("%d",&a);
     for(int i=0;i < a;i++){
         struct rohit r1;
-        r1.code;
-        printf("enter your %d person code: \n",i+1);
-        scanf("%d",&r1.code);
-        r1.salary;
-        printf("enter your %d person salary: \n",i+1);
-        scanf("%f",&r1.salary);
         r1.name;
         printf("enter your %d person name: \n",i+1);
         scanf("%s", r1.name);
-        
+         r1.salary;
+        printf("enter your %d person salary: \n",i+1);
+        scanf("%f",&r1.salary);
+        r1.code;
+        printf("enter your %d person code: \n",i+1);
+        scanf("%d",&r1.code);
+       
         printf("the value of code is:%d\n",r1.code);
          printf("the value of code is:%f\n",r1.salary);
-          printf("the value of code is:%d\n",r1.code);
+          printf("the value of code is:%d\n \n",r1.code);
         }    
         return 0;
     }
